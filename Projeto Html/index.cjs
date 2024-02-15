211491dfc9a042ff9e3b900063acf02b83ac027a
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2024-02-15 09:47:36 -0300

    Update index.cjs

--- Projeto Html/index.cjs
+++ Projeto Html/index.cjs
@@ -147,7 +147,6 @@ fetch(sheetUrl)
      /* const isConfiguredActive = isActive?.toLowerCase() === 'yes';       
      
      
-     
       const currentDate = new Date();
       const isDateInRange =
         currentDate >= new Date(startDate) && currentDate <= new Date(endDate);
