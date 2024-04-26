c1629ccf8221914a0a363c40f6db6bd851a89f9b
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2024-04-26 05:48:38 -0300

    Update index.cjs

--- Projeto Html/index.cjs
+++ Projeto Html/index.cjs
@@ -17,6 +17,7 @@ const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/value
 
 
 
+
 function removeAllSponsors(baseContentDir) {
   console.log('------------------------');
   console.log('Removing sponsors from: ', baseContentDir);
