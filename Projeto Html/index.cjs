317e6e4fe7c1d687e59dfe8b8593e2b11c28a43a
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2024-04-29 10:13:59 -0300

    Update index.cjs

--- Projeto Html/index.cjs
+++ Projeto Html/index.cjs
@@ -26,7 +26,6 @@ function removeAllSponsors(baseContentDir) {
   const contentDirPath = path.join(dataDirPath, baseContentDir);
 
 
-  
   const contentDir = fs.readdirSync(contentDirPath);
   
   contentDir.forEach((content) => {
